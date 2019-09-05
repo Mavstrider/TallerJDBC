d59c85548c9a3a2ab9ecf12d43a6c5eaa32391a6
admin <krylenko.fcul@gmail.com>
2019-09-05 18:08:14 +0100

    jdbc query

--- src/main/java/org/springframework/samples/petclinic/Ejercicios.java
+++ src/main/java/org/springframework/samples/petclinic/Ejercicios.java
@@ -1,42 +1,118 @@
 package org.springframework.samples.petclinic;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Ejercicios {
 
-    public static void ejercicio1(Connection connection, Statement statement) throws SQLException {
-        // 1. btener todos los clientes de la clinica y mostrar sus datos por pantalla
-    }
+	public static void ejercicio1(Connection connection, Statement statement) throws SQLException {
+		// 1. btener todos los clientes de la clinica y mostrar sus datos por pantalla
+		String sql = "SELECT * FROM owners";
+		ResultSet rs = statement.executeQuery(sql);
 
-    public static void ejercicio2(Connection connection, Statement statement) throws SQLException {
-        // 2. Insertarnos a nosotros como nuevo propietario de una mascota
+		while (rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("first_name");
+			String lastname = rs.getString("Last_name");
+			String adress = rs.getString("address");
+			String city = rs.getString("city");
+			String telephone = rs.getString("telephone");
 
-    }
+			//
+			System.out.println("Id:" + id);
 
-    public static void ejercicio3(Connection connection, Statement statement) throws SQLException {
-        // 3. Modificar nuestra ciudad por Sevilla
-        int numeroDeFilasModificadas = -1;
-        System.out.println("Se han modificado " + numeroDeFilasModificadas + " rows.");
+			System.out.println("name:" + name);
 
-    }
+			System.out.println("apelido" + lastname);
+			System.out.println("adress:" + adress);
+			System.out.println("cidade" + city);
+			System.out.println("telephone:" + telephone);
+			System.out.println("__________________");
+		}
 
-    public static void ejercicio4(Connection connection) throws SQLException {
-        // 4. Crear una variable de tipo String y buscar todos los Owner que coincidan en nombre o apellido.
+	}
 
-    }
+	public static void ejercicio2(Connection connection, Statement statement) throws SQLException {
+		// 2. Insertarnos a nosotros como nuevo propietario de una mascota
+		statement = connection.createStatement();
+		String sql = "INSERT INTO owners (first_name, last_Name,address,city,telephone)"
+				+ "VALUES ('Joao','Vasco','diracao','cidade','99999')";
+		int row = statement.executeUpdate(sql);
 
-    public static void ejercicio5(Connection connection) throws SQLException {
-        // 5. Crear a partir de las variables nombre, apellido, direccion, ciudad y teléfono (todas de tipo
-        // String), un nuevo owner
-        int numeroDeFilasModificadas = -1;
-        System.out.println("Se han modificado " + numeroDeFilasModificadas + " rows.");
+	}
 
-    }
+	public static void ejercicio3(Connection connection, Statement statement) throws SQLException {
+		// 3. Modificar nuestra ciudad por Sevilla
+		// int numeroDeFilasModificadas = -1;
+		// String sqlupdate = "UPDATE owners SET cidade=? WHERE id=?";
+		statement = connection.createStatement();
+		String sqlUpdate = "UPDATE owners " + "SET city = 'Sevilla' " + "WHERE first_name ='Joao'";
 
-    public static void reto(Connection connection, Statement statement) throws SQLException {
+		int numeroDeFilasModificadas = statement.executeUpdate(sqlUpdate);
 
-    }
+		System.out.println("Se han modificado " + numeroDeFilasModificadas + " rows.");
+
+	}
+
+	public static void ejercicio4(Connection connection) throws SQLException {
+		// 4. Crear una variable de tipo String y buscar todos los Owner que coincidan
+		// en nombre o apellido.
+
+		// SELECT *
+		// FROM suppliers
+		// WHERE (state = 'California' AND supplier_id <> 900)
+		// OR (supplier_id = 100);
+		String sql = "SELECT * FROM owners WHERE first_name =? OR last_name=?";
+		PreparedStatement ps = connection.prepareStatement(sql);
+		
+		ps.setString(1, "Joao");
+		ps.setString(2, "Black");
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			// Retrieve by column name
+			int id = rs.getInt("id");
+			String name = rs.getString("first_name");
+			String lastname = rs.getString("last_name");
+			String morada = rs.getString("address");
+			String city = rs.getString("city");
+			String telefone = rs.getString("telephone");
+
+			// Display values
+			System.out.println("id: " + id);
+			System.out.println("first_name: " + name);
+			System.out.println("last_name: " + lastname);
+			System.out.println("morada" + morada);
+			System.out.println("cidade" + city);
+			System.out.println("telefone" + telefone);
+		}
+	}
+
+	public static void ejercicio5(Connection connection) throws SQLException {
+		// 5. Crear a partir de las variables nombre, apellido, direccion, ciudad y
+		// teléfono (todas de tipo
+		// String), un nuevo owner
+		String SQL_INSERT = "INSERT INTO owners (first_name, last_name, address, city, telephone) VALUES (?,?,?,?,?)";
+		int numeroDeFilasModificadas = -1;
+		   PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT);
+
+	            preparedStatement.setString(1, "antonio");
+	            preparedStatement.setString(2, "con");
+	            preparedStatement.setString(3, "expo");
+	            preparedStatement.setString(4, "lisboa");
+	            preparedStatement.setString(5, "920349699");
+	           
+
+	            int row = preparedStatement.executeUpdate();
+		System.out.println("Se han modificado " + row + " rows.");
+		   
+
+	}
+
+	public static void reto(Connection connection, Statement statement) throws SQLException {
+
+	}
 
 }
